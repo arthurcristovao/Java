4ba719c41d6fd60edf7449e7b5a41dbca9ec48eb
Arthur Cristovão <49735882+arthurcristovao@users.noreply.github.com>
2023-11-09 19:41:01 -0300

    Update authortitle.sql

--- POO II/Aula15 - 14p2 - Arthur C/Aula15/SQL/authortitle.sql
+++ POO II/Aula15 - 14p2 - Arthur C/Aula15/SQL/authortitle.sql
@@ -2,7 +2,7 @@ use aula14;
 
 CREATE TABLE AuthorTitle (
 	atAuthorsID INTEGER,
-    atISBN INTEGER,
+    	atISBN INTEGER,
 	FOREIGN KEY ( atAuthorsID ) REFERENCES authors ( authorsID ),
 	FOREIGN KEY ( atISBN ) REFERENCES titles ( isbn )
 );
